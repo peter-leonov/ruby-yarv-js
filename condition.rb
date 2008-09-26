b769e625d35e9ca08b7eb5f5311e4c0c0ff43a02
Peter Leonov <gojpeg@gmail.com>
2008-09-26 17:40:57 +0000

    + local vars test

---
+++ condition.rb
@@ -0,0 +1,35 @@
+
+a = true
+
+if a
+  puts "true"
+end
+
+if a
+  puts "true"
+else
+  puts "false"
+end
+
+__END__
+
+[:branchunless, :label_28] UNKNOWN, [["$1_2"]]
+[:jump, :label_14] UNKNOWN, [["$1_2"]]
+[:label, :label_14], [["$1_2"]]
+
+[:jump, :label_28] UNKNOWN, [[]]
+[:label, :label_28], [[]]
+
+
+
+[:branchunless, :label_49] UNKNOWN, [["$1_2"]]
+[:jump, :label_36] UNKNOWN, [["$1_2"]]
+[:label, :label_36], [["$1_2"]]
+
+[:jump, :label_60] UNKNOWN, [[]]
+[:label, :label_49], [[]]
+
+[:label, :label_60], [["puts(\"false\")"]]
+
+
+[:leave], [["puts(\"false\")"]]
